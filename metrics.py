7e8998caa46566fc50bbafaea35a10208b195a08
s22a0061 <s22a0061@siswa.umk.edu.my>
2026-01-28 04:55:16 +0800

    Delete metrics.py

--- metrics.py
+++
@@ -1,6 +0,0 @@
-from sklearn.metrics import accuracy_score, classification_report
-
-def evaluate_model(y_true, y_pred):
-    acc = accuracy_score(y_true, y_pred)
-    report = classification_report(y_true, y_pred)
-    return acc, report
